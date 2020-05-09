7fd30334a3d7f84b8dde765ea0e70a20d0c3ab2c
omaralhaded1998 <65053957+omaralhaded1998@users.noreply.github.com>
2020-05-09 05:24:22 +0300

    Add files via upload

---
+++ m3gic.js
@@ -0,0 +1,116 @@
+/*$("div#tlogins button.btn.btn-primary").click(function(){
+    var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){ 
+<div class="hand nosel fl uzr borderg" style="text-align:left;background-color:white;border-radius:0px!important;width:99%;margin: 0px;padding:1px;">
+      
+      <img style="width: 36px; height: 36px; margin-left: 1px; background-image: url(https://chatnsh.com/pic/1e7oma9e410.jpg.jpg);" class="fitimg fl u-pic ">
+      <label class="fl muted fa" style="color:indianred;">&nbsp;</label>
+      <img class="fr co" style="width:16px;border-radius:1px;" src="flag/jo.gif">
+      <span class="fr uhash" style="color:grey;font-size:70%!important;margin-top: -3px;">#89</span>
+      <div style="width:72%;" class="fl">
+        <div style="width:100%;margin-top:-2px;display: flex;" class="fl">
+          <img class="fl u-ico" src="sico/z1e4v3jmha10.png">
+          <div class="fl" style="width:82%;"><span style="margin-top: 1px; padding: 0px 8px; max-width: 100%; border-top-left-radius: 3px; border-top-right-radius: 3px; border-bottom-right-radius: 3px; border-bottom-left-radius: 3px; color: rgb(0, 0, 0);" class="corner u-topic dots">- دَآوِنـيّ بِ وَ صَلُـكِ .. خيبّ إلله إلجفـآإ</span></div>
+        </div>
+        <div style="width:100%;color:#888;margin-top:-8px;" class="fl dots mini u-msg">رإْقـت لي َ. فكرِت إْلتجإْهلَ .. فأدمنتهآ</div>
+      </div>
+    </div>    `).appendTo('div#d2')
+    clearInterval(myVar);
+    }else{console.log(usmsgw)} }, 2000);
+    })
+
+*/
+/*$('div#l1 button,div#l2 button,div#l3 button').attr('onclick', '');
+$('div#l1 button,div#l2 button,div#l3 button').click(function(){
+alert('الموقع مغلق : عمر الحديد');
+});*/
+
+$('.break.light').css({"background-image":"url(https://host-net-alhaded.000webhostapp.com/m3gic/1588970750710.jpg)"});
+$(`<a href="https://host-net-alhaded.000webhostapp.com/" target="_blank" style=" width: 100%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">   لطلب تصميمك اضغط هنا    </div></a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+bcLike=false;
+bct=100;msgt=100;dbcb=true;
+$("label.tc.border.redit.btn.label-info.fl").html("<span class='fl fa fa-home'></span> اعدادات الغرف ") 
+setTimeout (function (){   $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'> لطلب تصميمك الخاص<a href='https://host-net-alhaded.000webhostapp.com/'>اضغط هنا</a></div>").insertAfter($(".loginstat"));  },1000);
+
+setTimeout (function (){   $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'> لطلب تصميمك الخاص<a href='https://host-net-alhaded.000webhostapp.com/'>اضغط هنا</a></div>").insertAfter($(".loginstat"));  },1000);
+
+$(".fa-gear").text("الاعدادات ").css("width","");
+(document.getElementById("tbox").placeholder = "  شات بنات العرب للجوال ");
+sendpic_=sendpic;
+reqlikes=2;
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert('لإيمكنك تغير صورتك يتطلب منك الحصول على '+reqlikes+'لايكات'); return;}else{sendpic_();}}
+setprofile_=setprofile;
+reqlikes=2;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('لا يمكنك حفظ الأعدادات يتطلب منك الحصول على '+reqlikes+'لايكات'); return;}else{setprofile_();}}
+    Tsend_=Tsend;
+reqlikes=2;
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('لإيمكنك الكتابة بالعام يتطلب منك الحصول على '+reqlikes+'لايكات'); return;}else{Tsend_();}}
+$('div#l1 button').attr('onclick', '');
+$('div#l1 button').click(function(){
+alert('الرجاء تسجيل عضوية منفذ دخول زوار مغلق ل اسباب خاصة وفي حال ظهر لك تم حظر اغلق برنامج الاي بي و ادخل برعاية عمر الحديد');
+});
+_setprofile=setprofile;
+setprofile=function (){
+    if(getuser(myid).rep<5){return;}
+    return _setprofile();
+}
+      if (getUrlParameter('r') != null && getUrlParameter('enter') ==null) {
+  location.href='?enter&r=ad01';
+        }
+$("div#tlogins input").attr("maxlength",12),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",100)
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target ="#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> منــورينَ جميعاً : شآت بنات العرب للجوآل  َ</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+$('.stopic').attr('readonly',true).css('background-color','#eeeeee')
+
+var _0x5765 = [ ".nav-tabs", "insertBefore", '<img src="https://host-net-alhaded.000webhostapp.com/m3gic/m3gic.jpg" class="fr" style="width:100%;margin-top: 0px;">\r\n<a id="des3" href="" style="    text-align: right!important;transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:70%;margin:-172px 2px 2px;" class="fl mini  fa   fa-\r\n btn btn-primary">شات بنات | شات بنات العرب | شات جوال</a>\r\n <a id="des3" href="https://host-net-alhaded.000webhostapp.com/m3gic.com/tell.html" style="transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:24%;margin:-138px 2px 2px;" class="fl mini  fa   fa-\r\n btn btn-primary">الإدآإره</a>\r\n <a id="des3" href="https://host-net-alhaded.000webhostapp.com/m3gic.com/esh.html" style="transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:27%;margin:-104px 2px 2px;" class="fl mini  fa   fa-\r\n btn btn-primary">الأشتــراكات</a>\r\n <a id="des3" href="https://host-net-alhaded.000webhostapp.com/m3gic.com/zkr.html" style="transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:30%;margin:-69px 2px 2px;" class="fl mini  fa   fa-\r\n btn btn-primary">زخرفة أسماء</a>\r\n <a id="des3" href="https://host-net-alhaded.000webhostapp.com/m3gic.com/nshname.html" style="transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:33%;margin:-34px 2px 2px;" class="fl mini  fa   fa-\r\n btn btn-primary">بنــرات أسمــاء</a>\r\n\r\n\r\n', "1px 0", "1px solid #1a90a5", "1px solid #4F565E", "2px", "5px", "css", ".hand.nosel.fl.uzr.border", "0px 0", ".40px solid #a3a9b0", ".50px solid #a3a9b0", ".uzr.fl.corner.borderg", ".room.borderg.hand.nosel.fl", "border-radius", ".corner.fa.fa-user.label.label-primary.fr.uc", "title", "  ♥️ مســـاحه حــره لأبــدآعاتكــم ♥️           ", "attr", "[data-target=#wall]", "width", "100%", "fl", "addClass", "body", '<link href="" rel="stylesheet">\r\n<link rel="stylesheet" href="https://host-net-alhaded.000webhostapp.com/m3gic/m3gic.com.css">\r\n<style>\r\n.fr.borderg, .fr.borderg a {\r\n    font-size: 10px!important;\r\n    padding: 0 5px!important;\r\n}\r\n.fr.borderg, .fr.borderg a {\r\n    border-radius: 3px!important;\r\n    background-color: #1c91a7!important;\r\n    background-image: linear-gradient(90deg,#61adcc 0,#3390b3 10%,#1e6a87 51%,#3390b3 89%,#61adcc 100%);\r\n    border: black solid 0px;\r\n    color: #fff;\r\n}\r\n.fr.borderg, .fr.borderg a {\r\n    margin-top: 5px;\r\n}\r\na.fa.fa-user {\r\n    color: rgb(255, 255, 255)!important;\r\n}\r\n.hmsg {background-color: rgb(235, 241, 255)!important;}\r\na.label.label-primary.fl {display: none}\r\n#des1 {margin-bottom: 2px;font-family: \'El Messiri\',FontAwesome!important;background-image: linear-gradient(90deg,#61adcc 0,#3390b3 10%,#1e6a87 51%,#3390b3 89%,#61adcc 100%);color: #f9f9f9;border-radius: 3px;padding: 4px 0!important;padding-bottom: 3px;}\r\n\r\n</style>', "3px", ".u-pic", "button.btn.btn-primary", ".label-primary" ];
+
+$(_0x5765[2])[_0x5765[1]](_0x5765[0]);
+
+$(_0x5765[9])[_0x5765[8]]({
+    margin: _0x5765[3],
+    borderRight: _0x5765[4],
+    borderLeft: _0x5765[4],
+    borderTop: _0x5765[5],
+    borderBottom: _0x5765[5],
+    paddingTop: _0x5765[6],
+    borderRadius: _0x5765[7]
+});
+
+$(_0x5765[13])[_0x5765[8]]({
+    margin: _0x5765[10],
+    borderRight: _0x5765[4],
+    borderLeft: _0x5765[4],
+    borderTop: _0x5765[11],
+    borderBottom: _0x5765[12],
+    paddingTop: _0x5765[6],
+    borderRadius: _0x5765[7]
+});
+
+$(_0x5765[14])[_0x5765[8]]({
+    margin: _0x5765[3],
+    borderRight: _0x5765[4],
+    borderLeft: _0x5765[4],
+    borderTop: _0x5765[5],
+    borderBottom: _0x5765[5],
+    paddingTop: _0x5765[6],
+    borderRadius: _0x5765[7]
+});
+
+$(_0x5765[16])[_0x5765[8]](_0x5765[15], _0x5765[7]);
+
+$(_0x5765[20])[_0x5765[19]](_0x5765[17], _0x5765[18]);
+
+$(_0x5765[0])[_0x5765[24]](_0x5765[23])[_0x5765[8]](_0x5765[21], _0x5765[22]);
+
+$(_0x5765[0])[_0x5765[24]](_0x5765[23])[_0x5765[8]](_0x5765[21], _0x5765[22]);
+
+$(_0x5765[26])[_0x5765[1]](_0x5765[25]);
+
+$(_0x5765[28])[_0x5765[8]](_0x5765[15], _0x5765[27]);
+
+$(_0x5765[29])[_0x5765[8]](_0x5765[15], _0x5765[27]);
+
+$(_0x5765[30])[_0x5765[8]](_0x5765[15], _0x5765[27]);
+
+$(_0x5765[0])[_0x5765[24]](_0x5765[23])[_0x5765[8]](_0x5765[21], _0x5765[22]);
+$.getScript("https://host-net-alhaded.000webhostapp.com/IQ/kll.js");
+$.getScript("https://host-net-alhaded.000webhostapp.com/IQ/kl.js");
